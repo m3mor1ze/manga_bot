c3e5362f168ce73a0fa51ad918140e4bda05acc6
poluzerovT <tpoluzerov@gmail.com>
2023-03-16 22:41:07 +0300

    run script

--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+echo 'RUN SCRIPT'
 
+echo 'GOING TO INSTALL REQUIREMENTS'
+python3 -m pip install --no-cache-dir -r requirements.txt
+echo 'SET UP REQUIREMENTS'
+
+echo 'RUNNING MAIN'
 python3 main.py
+
+echo 'STARTED. QUITING...'
+
