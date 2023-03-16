3834370cf86cd070c702d11633bc1d506a92e339
poluzerovT <tpoluzerov@gmail.com>
2023-03-16 17:17:14 +0300

    need parser for picture link scrapping

--- asyncscrap.py
+++ asyncscrap.py
@@ -1,19 +1,49 @@
-import asyncio
+from dataclasses import dataclass
+from typing import List
 from bs4 import BeautifulSoup
 import aiohttp
 
 
-async def scrap(url):
-    async with aiohttp.ClientSession('https://mintmanga.live', headers={'User-agent': 'Mozila'}) as session:
-        print(session)
+@dataclass
+class PictureRaw:
+    width: int
+    height: int
+    pic_url: str
 
-        async with session.get(url) as resp:
-            body = await resp.text()
-            soup = BeautifulSoup(body, 'html.parser')
 
-            # ЗДЕСЬ НАДО НАЙДИ ТЕГ img В КОТОРОМ ВСЕ ДАННЫЕ ПО КАРТИНКУ И ВЫТЯНУТЬ
-            # ССЫЛКУ НА КАРТИНКУ И ЕЕ РАЗМЕРЫ
-            x = soup.findAll('img', class_='manga-img_0 manga-img')
+@dataclass
+class MangaRaw:
+    name: str
+    url: str
+    length: int
+    pics_raw: List[PictureRaw]
 
-            print(x)
-            print(len(x))
+
+class MangaScrapper:
+    """
+    Provides interface for getting list of picture links
+    """
+
+    __base_url__ = 'https://mintmanga.live'
+    __headers__ = {
+        'User-agent': 'Mozila',
+        'Content-disposition': 'attachment',
+        # 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    }
+
+    async def ge_manga_raw(self, url: str) -> MangaRaw:
+        async with aiohttp.ClientSession(self.__base_url__, headers=self.__headers__) as session:
+            async with session.get(url) as resp:
+                body = await resp.text()
+                soup = BeautifulSoup(body, 'html.parser')
+
+        script = soup.find('div',
+                           class_='reader-controller pageBlock container reader-bottom bordered-page-block') \
+            .find('script', type='text/javascript')
+
+        content = script.text
+        manga_raw = self.parse_picture_object_from_script(content)
+        return manga_raw
+
+    def parse_picture_object_from_script(self, script: str) -> MangaRaw:
+        return script
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from asyncscrap import scrap
+from asyncscrap import MangaScrapper
 import asyncio
 
-URL = '/saga_o_vinlande__A35c96/vol1/1'
+async def example():
+    ls = MangaScrapper()
+    res = await ls.ge_manga_raw('/saga_o_vinlande__A35c96/vol1/1#page=5')
+    print(res)
 
 if __name__ == '__main__':
-    asyncio.run(scrap(URL))
+    asyncio.run(example())
